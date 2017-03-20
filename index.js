1c87d3971e803d801ad72b242ac9454908c8743d
NEUILLY\TiJiang <tijiang@deloitte.fr>
2017-03-20 20:13:43 +0100

    modified:   index.js 	modified:   package.json 	modified:   public/app.js 	modified:   public/index.html 	new file:   public/uploads.crypt 	new file:   public/uploads/.crypt 	new file:   "public/uploads/Calendrier des cong\303\251s et ponts 2016-2017.pdf" 	new file:   public/uploads/aventador.jpg 	new file:   public/uploads/bulletin.pdf 	new file:   public/uploads/cute.jpg 	new file:   public/uploads/logan.jpg

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');  //middleware of Express
 var through = require('through');  //the basis for most of the synchronous streams in event-stream
 var path = require('path');  //The path module provides utilities for working with file and directory paths
 require('dotenv').config(); //Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env.
+var formidable = require('formidable'); //Declare formidable var, for file uploading
 
 var storj = require('storj-lib');
 var storj_utils = require('storj-lib/lib/utils');
@@ -70,7 +71,8 @@ app.get('/buckets/list', function(req, res) {
 });
 
 
-/** Upload file **/
+/** Upload file (Original Version)**/
+/**TJI
 app.get('/files/upload', function(req, res) {
   separator();
   console.log('Retrieving buckets...')
@@ -80,17 +82,16 @@ app.get('/files/upload', function(req, res) {
       return console.error(err.message);
     }
 
+
     // Step 1a) Use the first bucket
     var bucketId = buckets[0].id;
     console.log('Uploading file to: ', bucketId);
 
     // Step 1b) Path of file
-
-    var filepath = './public/grumpy.jpg';
+    var filepath = './public/logan.jpg';
     console.log('Path of file: ', filepath);
-
     // Step 1c) Name of file
-    var filename = 'grumpy.jpg';
+    var filename = 'logan.jpg';
     console.log('Name of file: ', filename);
 
     // Step 2) Create a filekey with username, bucketId, and filename
@@ -140,6 +141,112 @@ app.get('/files/upload', function(req, res) {
       });
   });
 });
+**/
+
+//TJI Test Upload
+app.post('/upload', function(req, res){
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/public/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+
+    //TJI: BEGIN TO SEND FILE TO STORJ
+    separator();
+    console.log('Retrieving buckets...')
+    // Step 1
+    client.getBuckets(function(err, buckets) {
+      if (err) {
+        return console.error(err.message);
+      }
+
+
+      // Step 1a) Use the first bucket
+      var bucketId = buckets[0].id;
+      console.log('Uploading file to: ', bucketId);
+
+      // Step 1b) Path of file
+      var filepath = path.join(__dirname, '/public/uploads/',file.name);
+      console.log('Path of file: ', filepath);
+      // Step 1c) Name of file
+      //var filename =file.name;
+      var filename =file.name;
+      console.log('Name of file: ', filename);
+
+      // Step 2) Create a filekey with username, bucketId, and filename
+      var filekey = getFileKey(STORJ_EMAIL, bucketId, filename);
+
+      // Step 3) Create a temporary path to store the encrypted file
+      var tmppath = filepath + '.crypt';
+
+      // Step 4) Instantiate encrypter
+      var encrypter = new storj.EncryptStream(filekey);
+
+      // Step 5) Encrypt file
+      fs.createReadStream(filepath)
+        .pipe(encrypter)
+        .pipe(fs.createWriteStream(tmppath))
+        .on('finish', function() {
+          console.log('Finished encrypting');
+
+          // Step 6) Create token for uploading to bucket by bucketId
+          client.createToken(bucketId, 'PUSH', function(err, token) {
+            if (err) {
+              console.log('error', err.message);
+            }
+            console.log('Created token', token.token);
+
+            // Step 7) Store the file
+            console.log('Storing file in bucket...');
+            client.storeFileInBucket(bucketId, token.token, tmppath,
+              function(err, file) {
+                if (err) {
+                  return console.log('error', err.message);
+                }
+                console.log('Stored file in bucket');
+                // Step 8) Clean up and delete tmp encrypted file
+                console.log('Cleaning up and deleting temporary encrypted file...');
+                fs.unlink(tmppath, function(err) {
+                  if (err) {
+                    return console.log(err);
+                  }
+                  console.log('Temporary encrypted file deleted');
+                });
+
+                console.log(`File ${filename} successfully uploaded to ${bucketId}`);
+                res.status(200).send(file);
+              });
+          });
+        });
+    });
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+
+
+
+});
 
 
 var localAssetsDir = __dirname + '/public';
@@ -181,4 +288,14 @@ function generateMnemonic() {
     console.log('Mnemonic written to .env file. Make sure to add this to heroku config variables with \'heroku config:set STORJ_MNEMONIC="<VALUE FROM .ENV FILE>\'');
     return;
   }
+}
+
+function getFileKey(user, bucketId, filename) {
+  console.log('Generating filekey...')
+  generateMnemonic();
+  var realBucketId = storj_utils.calculateBucketId(user, bucketId);
+  var realFileId = storj_utils.calculateFileId(bucketId, filename);
+  var filekey = keyring.generateFileKey(realBucketId, realFileId);
+  console.log('Filekey generated!');
+  return filekey;
 }
